026e954e451e2522e8c862fe36088c4e8c9cc126
Dmitry <hellsigner@gmail.com>
2015-06-04 16:06:43 +0300

    Added example config file

---
+++ dist/dbconfig.local.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Place it into your config/autoload directory, or define database config in your own file.
+ */
+return array(
+    'db' => array(
+        'driver'   => 'Pdo_Mysql',
+        'database' => 'skynet',
+        'username' => 'root',
+        'password' => ''
+    )
+);
